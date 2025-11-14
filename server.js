b7d8bcf4fc59ccda423038b3366da24d87c265e1
junodofbelhaven <inceanilabdullah@gmail.com>
2025-11-15 02:04:34 +0300

    start script added

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
